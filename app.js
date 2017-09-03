27c64c45637b491ea77bb051d2c5f415b6c60493
stefferd <vandenberg.stef@gmail.com>
2017-09-03 16:12:40 +0200

    Only init the mailer path

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 // var sassMiddleware = require('node-sass-middleware');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// var index = require('./routes/index');
+// var users = require('./routes/users');
 var mailer = require('./routes/mailer');
 
 // import environmental variables from our variables.env file
@@ -27,8 +27,8 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+// app.use('/users', users);
 app.use('/mailer', mailer);
 
 // catch 404 and forward to error handler
